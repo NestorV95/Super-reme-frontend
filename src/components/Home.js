45ca1887c6924c303033025d5c9895e00ec61819
Nestor <nestor.venegas95@gmail.com>
2021-05-09 02:03:56 -0500

    organized home related code

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,9 @@
 import React from "react";
-import {FaLinkedin,FaGithubSquare,FaTwitterSquare,FaYoutubeSquare} from "react-icons/fa";
 import { IconContext } from "react-icons";
+import {FaLinkedin,FaGithubSquare,FaTwitterSquare,FaYoutubeSquare} from "react-icons/fa";
+import { Link } from "react-router-dom";
 import "../Home.css";
-
 import LogoCdt from "./Logo.js";
-import HomeNav from "./HomeNav.js";
 
 const Home = () => {
   let cdtStyle = { color: "white" };
@@ -20,6 +19,40 @@ const Home = () => {
         <HomeSocial />
       </div>
     </div>
+  );
+};
+
+const HomeNav = () => {
+  return (
+      <ul className="hn-ul">
+        <li className="hn-li">
+          <Link className="hn-a"  to="/news">News</Link>
+        </li>
+        <li className="hn-li">
+          <Link className="hn-a" to="/previews">Line Up Preview</Link>
+        </li>
+        <li className="hn-li">
+          <Link className="hn-a" to="/lookbooks">Lookbook</Link>
+        </li>
+        <li className="hn-li">
+          <Link className="hn-a" to="/shop">Shop</Link>
+        </li>
+        <li className="hn-li">
+          <Link className="hn-a" to="/random">Random</Link>
+        </li>
+        <li className="hn-li">
+          <Link className="hn-a" to="/about">About</Link>
+        </li>
+        <li className="hn-li">
+          <Link className="hn-a" to="/stores">Stores</Link>
+        </li>
+        <li className="hn-li">
+          <Link className="hn-a" to="/contact">Contact</Link>
+        </li>
+        <li className="hn-li">
+          <Link className="hn-a" to="/mailinglist">Mailing List</Link>
+        </li>
+      </ul>
   );
 };
 
