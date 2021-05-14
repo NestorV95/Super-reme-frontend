bc450f51d50a4510b81d6437ad53ff5547dfe464
Nestor <nestor.venegas95@gmail.com>
2021-05-14 04:56:58 -0500

    finishing touches on mailing list

--- src/pages/MailingList.js
+++ src/pages/MailingList.js
@@ -1,5 +1,5 @@
 //----------------------------------------- packages ---------------------------------------------//
-import React from "react";
+import React,{useState} from "react";
 //---------------------------------------- components --------------------------------------------//
 import BottomNav from "../components/BottomNav";
 import LogoCdt from "../components/Logo";
@@ -7,16 +7,40 @@ import LogoCdt from "../components/Logo";
 import '../styles/MailingList.css'
 //------------------------------------- Mailing List page ----------------------------------------//
 const MailingList=()=>{
+    const [syke,setSyke] = useState(false)
+
+    const simage = 'https://i.pinimg.com/originals/1b/24/d1/1b24d18d4a1405776d0f7450a979356b.jpg'
+    const syStyle = {
+        position: 'absolute',
+        display: 'block',
+        height: '50vh',
+        width: '50vw',
+        top: '30vh',
+        left: '0',
+        right: '0',
+        margin: 'auto'
+    }
+
+    const gotEm=()=>{
+        setSyke(true)
+    }
+
     return(
-        <div className="mailing-page">
+        <div className="mailing-page" >
             <LogoCdt/>
-            <MailingListForm/>
+            { syke? 
+                <img src={simage} alt=""style={syStyle}/>
+                :
+                <MailingListForm gotEm={gotEm}/>
+            }
+            
             <BottomNav/>
         </div>
     )
 }
 //---------------------------------- Mailing List Components -------------------------------------//
-const MailingListForm=()=>{
+const MailingListForm=({gotEm})=>{
+
     return(
         <div className="mlf-div">
             <div className="mlft1-div">
@@ -40,10 +64,12 @@ const MailingListForm=()=>{
                 </p>
             </div>
             <input className="cb-div"type="checkbox"></input>
-            <button className="sub">subscribe</button>
-            <button className="unsub">unsubscribe</button>
+            <button className="sub" onClick={()=>gotEm()}>subscribe</button>
+            <button className="unsub" onClick={()=>gotEm()}>unsubscribe</button>
         </div>
     )
 }
+
+
 
 export default MailingList
